ca28de4d957b7243045d8179add28a40ec4f3994
rahul bendale <rahul.bendale@ooma.com>
2022-01-30 20:37:11 +0530

    changes in crudapi.sql

--- crudapi.sql
+++ crudapi.sql
@@ -2,7 +2,7 @@ USE crudapi;
 
 CREATE TABLE tbl_employee
 (
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
     gender VARCHAR(255),
     department VARCHAR(255),
